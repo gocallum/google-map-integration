5ded50007b24e2496406f9ea81991deef4107935
ohemjake <mmartin76@ymail.com>
2024-09-10 14:23:18 +1000

    Add shadcn/Carousel in photo display

--- app/components/RestaurantAutocompleteSearch.tsx
+++ app/components/RestaurantAutocompleteSearch.tsx
@@ -9,6 +9,7 @@ import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { fetchNearbyRestaurants, VenueOption, getPhotoUrl, getCityList, getPlaceDetails, getCityFromCoordinates } from '@/app/actions/searchmap'; // Import getPhotoUrl
 import GoogleMap from './map';
 import { FaClock, FaMapMarkerAlt, FaChevronDown } from 'react-icons/fa';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
 
 export default function RestaurantSearch() {
   const [lat, setLat] = useState<number | null>(null);
@@ -29,7 +30,6 @@ export default function RestaurantSearch() {
     setIsDropdownOpen(false);
     navigator.geolocation.getCurrentPosition(
       async (position) => {
-        console.log("GEOLOC:", position);
         setLat(position.coords.latitude);
         setLon(position.coords.longitude);
 
@@ -81,7 +81,7 @@ export default function RestaurantSearch() {
     try {
       const location = `${lat},${lon}`;
       const results = await fetchNearbyRestaurants(inputValue, location);
-      console.log('Results:', results);
+      console.log('Restaurants Results:', results);
       setOptions(results);
       setIsDropdownOpen(true);
     } catch (error) {
@@ -99,8 +99,6 @@ export default function RestaurantSearch() {
       const searchType = isPostcode(inputValue) ? 'postcode' : 'city';
       const data = await getCityList(inputValue, searchType); // Pass searchType to the API
 
-      console.log("CITY/POSTCODE:", data);
-
       return data.predictions.map((prediction: any) => ({
         label: prediction.description,
         value: prediction.place_id,
@@ -116,10 +114,22 @@ export default function RestaurantSearch() {
     setSelectedVenue(option);
     setSearchTerm(option.name);
     setIsDropdownOpen(false);
+    console.log('Photo >> :', option.photos);
+
+    // const optionTest =
+    // {
+    //   "photos": [
+    //     "AXCi2Q42VA0p4aSo74rmlTzZYIhZ8XMdS20D5Jw7_4llMZfw3NkuvR4DcQ_o2jy2Ngbo92fGQqBHUYnD5s8fcKdXt9Jnwh-V7noInuA6T0kNSVPSSjkDR6ckFGmaFlycJNtWybHMEbXC39B1DBnTQMlSImAl3Rcn5Yv9Ymp0mC5KywzjUIAF",
+    //     "AXCi2Q5of7dJsKCkANQl44IS1Z6wLQ1IFkCfJAy7UzWUnAWlbI_bGy7SCCS672rgfqyyUy07y92Odng6kwLyQkgyIx2EF5eQpqTpGK0ExXU0CuV_1FtZwVSl6Vv-AsnOz-s3hMnRuaHXtcTj1Tm5WFxyJ4hcngZ5HBPXzuj0FwbBSW_6ZBtu",
+    //     "AXCi2Q5I-sESFKcc3N6tFEFAkMqoRGJhUo_a4TmVzgWGDG0H2TyMtkn7LzSYbkMJnGSDfNY5gPZARZJZpsloymRCtbsUUH0_PdSjSOOR9HuMj-3fNGI7erCDXpx1RF15qU-H274ExqgEZDS3L_UY4zuu2fMQCpzBD-Cyz6KLaifLxdLdWS6p",
+    //     "AXCi2Q7rJpbMF8rZCd1om3a-jBQAI1tlrkoOIuBfYbpxuhD-9TvEVCKj_p8y0EKjodd0okl_HZ7km2RM7APEj0Xpaw0_37HE0m9FrYAn2SKZicUD51GbsAgSEgPas7yoPWHf0BLSH4WDK8UVRv_f8rW_nj3w48-XYdpzPobC-NEp_HjWOcfY"
+    //   ],
+    // };
 
     if (option.photos.length > 0) {
       // Fetch URLs for the photos
       const urls = await Promise.all(option.photos.map((photo) => getPhotoUrl(photo)));
+      console.log('Photo URLs:', urls);
       setPhotoUrls(urls); // Update the state with the resolved photo URLs
     } else {
       setPhotoUrls([]);
@@ -250,16 +260,21 @@ export default function RestaurantSearch() {
               <p className="font-medium">{selectedVenue.name}</p>
               <p className="text-sm text-gray-600">{selectedVenue.vicinity}</p>
               <p className="text-sm text-gray-600">{selectedVenue.distance.toFixed(1)} km away</p>
-              <div className="flex flex-wrap mt-4">
+              <div className="mt-4">
                 {photoUrls.length > 0 ? (
-                  photoUrls.map((url, index) => (
-                    <img
-                      key={index}
-                      src={url} // Use the resolved URL from the state
-                      alt={`Restaurant ${index + 1}`}
-                      className="w-64 h-64 object-cover mr-4 mb-4"
-                    />
-                  ))
+                  <Carousel>
+                    <CarouselContent>
+                      {photoUrls.map((url, index) => (
+                        <CarouselItem className="md:basis-1/2 lg:basis-1/3" key={index}>
+                          <img
+                            src={url}
+                            alt={`Restaurant ${index + 1}`}
+                            className="w-full h-full object-cover"
+                          />
+                        </CarouselItem>
+                      ))}
+                    </CarouselContent>
+                  </Carousel>
                 ) : (
                   <p>No photos available</p>
                 )}
